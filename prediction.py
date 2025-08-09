9d2fc4198b26224353bd6b433fc9eb97624622ad
chandu16 <jagadishdevu523@icloud.com>
2025-08-09 18:52:58 +0530

    Final Changes

--- prediction.py
+++ prediction.py
@@ -22,39 +22,32 @@ cat_cols = [
     'loan_intent', 'previous_loan_defaults_on_file'
 ]
 
-# ----------- Take CLI Inputs -------------
+# ----------- Take Inputs -------------
 print("\n=== Loan Approval Prediction ===")
 user_data = {}
 
-# Numeric inputs
 for col in num_cols:
     user_data[col] = float(input(f"Enter {col}: "))
 
-# Categorical inputs
 for col in cat_cols:
     user_data[col] = input(f"Enter {col}: ")
 
-# Create DataFrame
 df_input = pd.DataFrame([user_data])
 
 # ----------- Preprocessing -------------
-# Scale numeric features (order matters!)
 df_input[num_cols] = scaler.transform(df_input[num_cols])
 
-# Encode categorical features (order matters!)
 df_cat_encoded = pd.DataFrame(
     encoder.transform(df_input[cat_cols]),
     columns=encoder.get_feature_names_out(cat_cols),
     index=df_input.index
 )
 
-# Combine scaled nums + encoded cats
 X_final = pd.concat([df_input[num_cols], df_cat_encoded], axis=1)
 
 # ----------- Prediction -------------
 probability = logit_model.predict_proba(X_final)[:, 1][0]
 prediction = int(probability >= tuned_threshold)
 
-# ----------- Output -------------
 print(f"\nPredicted probability of loan approval: {probability:.4f}")
 print(f"Decision : {'APPROVED' if prediction == 1 else 'DENIED'}")
